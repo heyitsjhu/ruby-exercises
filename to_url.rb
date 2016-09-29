4017fa6b1d7dc51ce0abbecb8fafe3a076bfe65e
Johnny Hu <jgeehen.hu@gmail.com>
2016-09-29 16:33:15 -0700

    Add more special characters to gsub; add comments

--- to_url.rb
+++ to_url.rb
@@ -1,12 +1,23 @@
-## The following method will take a string
-## and convert it into a URL friendly format
-## by removing leading and trailing spaces,
-## subbing spaces between words with hyphens,
-## and removing apostrophes.
+# Converts a string into a URL-friendly format.
 
 def to_url(string)
-  string.strip
+  string.strip             # strips leading and trailing whitespaces
         .downcase
-        .gsub(/'+/, '')
-        .gsub(/\s+/, "-")
+        .gsub(/[.,:;'"?!]/, '') # removes the following characteres: .,:;'"?!
+        .gsub(/\s+/, "-")   # replace spaces with hyphens
+end
+
+examples = {
+                  # Returns: her-mothers-favorite-quote-who-dat
+  :example_one    => to_url(" Her Mother's Favorite Quote: 'Who Dat?'"),
+
+                  # Returns blasphemy-i-cant-believe-it-why
+  :example_two    => to_url("Blasphemy! I can't BELIEVE it! Why!?"),
+
+                  # what-it-means-to-be-born-on-july-10-2000
+  :example_three  => to_url("    What It Means to be born on July 10, 2000...")
+}
+
+examples.each do |example, string|
+  puts string
 end
